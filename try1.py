48be52391667d8f4f9ac74916cf5c6451936dbaa
heyscottn <heyscottn@gmail.com>
2024-01-17 12:34:29 -0600

    edits

--- try1.py
+++ try1.py
@@ -1,12 +1,19 @@
-# part 1 - echo
+# part 1 - eextra
+# the input is a single word
+# the output is the word with an extra first and last letter
+# example in: word
+# example out: wwordd
+
+
+# part 2 - echo
 # the input is a single word, the output is that word repeated 3 times
 # ex input: hello 
 # ex output: hellohellohello
 
-# part 2 - large sum
+# part 3 - large sum
 # find the sum of all 3 digit integers
 
-# part 3 - every other
+# part 4 - every other
 # the input is a message, the output is every other letter
 # ex input: forescore and seven years ago
 # ex output: frsoeadsvnyasao
@@ -14,11 +21,14 @@
 choice = 'banana'
 # while choice != 'q' and choice != 'quit':
 while choice[0] != 'q':
+    message += "\n'extra' for eextraa"
     message = "\n'echo' for echo"
     message += "\n'sum' for large sum"
     message += "\n'other' for every other letter: "
     choice = input(message)
     print()
+    # TODO 'extra'
+    
     # TODO 'echo'
 
     # TODO 'sum'
